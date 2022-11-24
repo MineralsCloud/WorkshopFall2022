5c5bb0699bae8006846e13de2881752da21f6184
singularitti <singularitti@outlook.com>
2022-11-23 22:21:52 -0500

    Update qz/setup_julia.sh

--- qz/setup_julia.sh
+++ qz/setup_julia.sh
@@ -7,6 +7,16 @@ juliaup default release
 # Install Julia packages
 julia setup.jl
 
+FILE=~/.julia/bin/qe
+if test -f "$FILE"; then
+    echo "$FILE is installed."
+fi
+
+FILE=~/.julia/bin/xps
+if test -f "$FILE"; then
+    echo "$FILE is installed."
+fi
+
 # Configure Julia
 mkdir -p ~/.julia/config
 cp startup.jl ~/.julia/config
