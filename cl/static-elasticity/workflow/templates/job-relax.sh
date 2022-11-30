717897db3a09e288fa2a9f22a7dde4abfa9004c4
Chenxing <chenxing.luo@gmail.com>
2022-11-30 01:19:31 -0500

    Fix -npool=4

--- cl/static-elasticity/workflow/templates/job-relax.sh
+++ cl/static-elasticity/workflow/templates/job-relax.sh
@@ -16,4 +16,4 @@ module load openmpi
 module load quantum-espresso/6.7.0-openblas
 
 ### Run QE
-mpirun --map-by core --mca btl_openib_if_include "mlx5_2:1" --mca btl openib,self,vader pw.x -input relax.in -npool 4 1> relax.out 2> relax.err
+mpirun --map-by core --mca btl_openib_if_include "mlx5_2:1" --mca btl openib,self,vader pw.x -input relax.in 1> relax.out 2> relax.err
