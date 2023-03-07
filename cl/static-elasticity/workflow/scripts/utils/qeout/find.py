dd6808b56c13208e041c8db0bcfbdd4193584779
Michael <matambo4999@gmail.com>
2023-03-07 14:12:44 +0100

    update strings
    these are not exec objects with stdout

--- cl/static-elasticity/workflow/scripts/utils/qeout/find.py
+++ cl/static-elasticity/workflow/scripts/utils/qeout/find.py
@@ -7,32 +7,32 @@ __all__ = ["find_energy", "find_pressure", "find_final_energy", "find_structure"
 
 def find_energy(fname):
     ret = sh.grep("!", fname)
-    return float(re.search(r"(-?\d+\.?\d+)*\s+Ry", ret.stdout.decode()).group(1))
+    return float(re.search(r"(-?\d+\.?\d+)*\s+Ry", ret).group(1))
 
 def find_final_energy(fname):
     ret = sh.grep("-m1", "!", _in=sh.tac(fname))
-    return float(re.search(r"(-?\d+\.?\d+)*\s+Ry", ret.stdout.decode()).group(1))
+    return float(re.search(r"(-?\d+\.?\d+)*\s+Ry", ret).group(1))
 
 def find_pressure(fname):
     ret = sh.grep("P=", _in=sh.tac(fname))
-    return float(re.search(r"P=\s*(-?\d+\.?\d+)", ret.stdout.decode()).group(1))
+    return float(re.search(r"P=\s*(-?\d+\.?\d+)", ret).group(1))
 
 def find_final_pressure(fname):
     ret = sh.grep("P=", "-m1", fname)
-    return float(re.search(r"P=\s*(-?\d+\.?\d+)", ret.stdout.decode()).group(1))
+    return float(re.search(r"P=\s*(-?\d+\.?\d+)", ret).group(1))
 
 def find_volume(fname):
     ret = sh.grep("new unit-cell volume", _in=sh.grep("Begin final coordinates", "-A", 1, fname))
-    return float(re.search(r"=\s+(\S+)\s+a\.u\.\^3", ret.stdout.decode()).group(1))
+    return float(re.search(r"=\s+(\S+)\s+a\.u\.\^3", ret).group(1))
 
 
 def find_structure(fname, nat: int):
     ret = sh.grep("CELL_PARAMETERS", "-A", 3, fname)
-    _ret = "\n".join(ret.stdout.decode().split("\n")[-5:])
+    _ret = "\n".join(ret.split("\n")[-5:])
     cell_unit = _ret.split("\n")[0].strip().split()[1][1:-1]
     lattice_parameter = numpy.loadtxt(io.StringIO(_ret), skiprows=1)
     ret = sh.grep("ATOMIC_POSITIONS", "-A", nat, fname)
-    _ret = "\n".join(ret.stdout.decode().split("\n")[-nat-2:])
+    _ret = "\n".join(ret.split("\n")[-nat-2:])
     atom_unit = _ret.split("\n")[0].strip().split()[1][1:-1]
     unit_cell = []
     for line in _ret.split("\n")[1:-1]:
